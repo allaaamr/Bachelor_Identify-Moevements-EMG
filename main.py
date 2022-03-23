9fe55e9440f2159738db5c51d0162d61901d1450
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2022-03-23 14:27:28 +0200

    Plotting and Viewing Features

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import scipy.io
 import math
+import matplotlib.pyplot as plt
 
 
 def consecutive(data, stepsize=1):
@@ -74,7 +75,37 @@ for m in range(1, 51):
 #Convert dictionary to dataframe
 dff = pd.DataFrame.from_dict(Movements)
 # print(dff)
-# print(dff.loc['Electrode1'])
+# print(dff.loc['Electrode1']['Movement1'])
+
+fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8)) = plt.subplots(2,4)
+fig.suptitle('Electrode 1 Movement 2')
+
+ax1.set_title('R1')
+ax1.plot(dff.loc['Electrode1']['Movement2']['R1'])
+
+ax2.set_title('R2')
+ax2.plot(dff.loc['Electrode1']['Movement2']['R2'])
+
+ax3.set_title('R3')
+ax3.plot(dff.loc['Electrode1']['Movement2']['R3'])
+
+ax4.set_title('R4')
+ax4.plot(dff.loc['Electrode1']['Movement2']['R4'])
+
+ax5.set_title('R5')
+ax5.plot(dff.loc['Electrode1']['Movement2']['R5'])
+
+ax6.set_title('R6')
+ax6.plot(dff.loc['Electrode1']['Movement2']['R6'])
+
+ax7.set_title('R7')
+ax7.plot(dff.loc['Electrode1']['Movement2']['R7'])
+
+ax8.set_title('R8')
+ax8.plot(dff.loc['Electrode1']['Movement2']['R8'])
+
+
+plt.show()
 
 ## Features
 def rms(arr):
@@ -85,6 +116,7 @@ def rms(arr):
     mean = (square / (float)(n))
     root = math.sqrt(mean)
     return root
+
 def mav(arr):
     n = len(arr)
     absSum = 0
@@ -92,6 +124,7 @@ def mav(arr):
         absSum += abs(arr[i])
     mav = (absSum / (float)(n))
     return mav
+
 def var(arr):
     n = len(arr)
     square = 0
@@ -100,18 +133,22 @@ def var(arr):
 
     result = (square / (float)(n))
     return result
+
 def wl(arr):
     n = len(arr)
     sum=0
     for i in range(1, n):
         sum += abs(arr[i]-arr[i-1])
     return sum
+
 def iav(arr):
     n = len(arr)
     absSum = 0
     for i in range(0, n):
         absSum += abs(arr[i])
     return absSum
+
+
 #Extracting Features
 electrodes = {}
 for e in range(1, 11):
@@ -154,10 +191,59 @@ for e in range(1, 11):
 # Features = {"RMS": applyFeature((1)), "MAV": applyFeature(2)}
 # #Convert dictionary to dataframe
 
-df = pd.DataFrame.from_dict(electrodes)
+# df = pd.DataFrame.from_dict(electrodes)
+# E1M1 = pd.DataFrame.from_dict(df['Electrode1']['Movement1'])
+# E1M2 = pd.DataFrame.from_dict(df['Electrode1']['Movement2'])
+# E1M3 = pd.DataFrame.from_dict(df['Electrode1']['Movement3'])
+# E1M4 = pd.DataFrame.from_dict(df['Electrode1']['Movement4'])
+# E1M5 = pd.DataFrame.from_dict(df['Electrode1']['Movement5'])
+# E1M6 = pd.DataFrame.from_dict(df['Electrode1']['Movement6'])
+#
+# print(E1M1)
+# stats_df = E1M1.describe()
+# stats_df.loc['range'] = stats_df.loc['max'] - stats_df.loc['min']
+# out_fields = ['mean','25%','50%','75%', 'range']
+# stats_df = stats_df.loc[out_fields]
+# stats_df.rename({'50%': 'median'}, inplace=True)
+# print(stats_df)
+
+# ax = plt.axes()
+#
+# ax.scatter(E1M1.IAV, E1M1.MAV)
+#
+# # Label the axes
+# ax.set(xlabel='IAV  (cm)',
+#        ylabel='MAV  (cm)',
+#        title='IAV vs MAV');
+#
+# plt.show()
+
+# fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
+# fig.suptitle('RMS')
+#
+# ax1.set_title('Electrode 1 Movement 1')
+# ax1.plot(E1M1['RMS'])
+#
+# ax2.set_title('Electrode 1 Movement 2 ')
+# ax2.plot(E1M2['RMS'])
+#
+# ax3.set_title('Electrode 1 Movement 3 ')
+# ax3.plot(E1M3['RMS'])
+#
+# ax4.set_title('Electrode 1 Movement 4 ')
+# ax4.plot(E1M4['RMS'])
+#
+# ax5.set_title('Electrode 1 Movement 5 ')
+# ax5.plot(E1M5['RMS'])
+#
+# ax6.set_title('Electrode 1 Movement 6 ')
+# ax6.plot(E1M6['RMS'])
+#
+# plt.show()
+#ax.scatter(data.sepal_length, data.sepal_width)
 #dfRMS.to_csv(r'\df.csv', index = False)
 #pretty(electrodes)
-pretty(df['Electrode4']['Movement9'])
+#pretty(df['Electrode4']['Movement9'])
 # df = pd.DataFrame.from_dict(Features)
 # df = pd.DataFrame.from_dict(Features)
 # print(df['RMS'])
